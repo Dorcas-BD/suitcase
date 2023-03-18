68d3638982e3980b81173ee891ab6a6c62009ba8
Redeem Grimm <45304978+Redeem-Grimm-Satoshi@users.noreply.github.com>
2023-03-18 18:35:18 +0100

    Update TestCode.java

--- TestCode.java
+++ TestCode.java
@@ -8,7 +8,7 @@ public class TestCode{
 
 public static void main(String[] args) {
     
-    //This is CircleCLI Test Code
+    //This is CircleCLI Test Code Build Circle
 
     String message="CircleCLI Build Done!";
     System.out.println(" " + buildResult(message));
